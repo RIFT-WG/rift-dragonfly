4c72090bc868cc9e5b8f1d5267543aa498ccf7dc
prz@zeta2.ch <prz@zeta2.ch>
2023-10-19 15:11:56 +0200

    first SVG stuff

--- build.sh
+++ build.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
-D=draft-przygienda-rift-dragonfly++
+if [ `which xmlstarlet` -a `which xml2rfc` -a `which svgcheck` ]
+then
 
-# xml2rfc $D.xml
+  for f in svg/*.svg
+  do
+    cp $f $f.orig
+#    rm $f.prep
+#
+#    xmlstarlet ed -d "//sodipodi:namedview" $f | xmlstarlet ed -d "//svg:metadata" | \
+#    xmlstarlet ed -d "//@inkscape:version" | xmlstarlet ed -d "//@sodipodi:docname" | \
+#    xmlstarlet ed -d "//svg:g/@inkscape:label" | xmlstarlet ed -d "//svg:g/@inkscape:groupmode" | \
+#    # xmlstarlet ed -d "//svg:rect/@style" | \
+#    xmlstarlet ed -d "//svg:path/@style" | \
+#    xmlstarlet ed -d "//svg:text/@style" | \
+#    xmlstarlet ed -d "//svg:text/svg:tspan"  > $f.prep
+    svgcheck -r $f | xmlstarlet ed -d "//svg:text/@stroke" > $f.prep
+    # rm $f.1
+    done
 
-echo -- df++
+  D=draft-przygienda-rift-dragonfly++
 
+  # xml2rfc $D.xml
 
-xml2rfc $D.xml
+  echo -- df++
 
-nl -ba $D.txt > $D.nl.txt
+  echo -- to text
+  xml2rfc -N $D.xml
+  echo -- to pdf
+  xml2rfc --pdf $D.xml
+
+  nl -ba $D.txt > $D.nl.txt
+  exit
+fi
+
+echo install xml2rfc and xmlstarlet
+
 
