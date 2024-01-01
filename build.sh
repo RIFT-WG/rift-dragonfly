25f4b4316c760625996e3d20fdec511582a44198
prz@zeta2.ch <prz@zeta2.ch>
2024-01-01 22:39:12 +0100

    ASCII figures

--- build.sh
+++ build.sh
@@ -6,16 +6,21 @@ then
   for f in svg/*.svg
   do
     cp $f $f.orig
-#    rm $f.prep
-#
-#    xmlstarlet ed -d "//sodipodi:namedview" $f | xmlstarlet ed -d "//svg:metadata" | \
-#    xmlstarlet ed -d "//@inkscape:version" | xmlstarlet ed -d "//@sodipodi:docname" | \
-#    xmlstarlet ed -d "//svg:g/@inkscape:label" | xmlstarlet ed -d "//svg:g/@inkscape:groupmode" | \
-#    # xmlstarlet ed -d "//svg:rect/@style" | \
-#    xmlstarlet ed -d "//svg:path/@style" | \
-#    xmlstarlet ed -d "//svg:text/@style" | \
-#    xmlstarlet ed -d "//svg:text/svg:tspan"  > $f.prep
-    svgcheck -a -r $f | xmlstarlet ed -d "//svg:text/@stroke" > prep/$f
+     rm prep/$f
+
+
+#     xmlstarlet ed -d "//svg:g/@inkscape:label" | xmlstarlet ed -d "//svg:g/@inkscape:groupmode" | \
+#     xmlstarlet ed -d "//svg:rect/@style" | \
+#     xmlstarlet ed -d "//svg:path/@style" | \
+#     xmlstarlet ed -d "//svg:text/@style" | \
+#     xmlstarlet ed -d "//svg:text/svg:tspan"  | \
+
+    svgcheck -a -r $f | \
+    xmlstarlet ed -d "//sodipodi:namedview" | xmlstarlet ed -d "//svg:metadata" | \
+    xmlstarlet ed -d "//@inkscape:version" | xmlstarlet ed -d "//@sodipodi:docname" | \
+    xmlstarlet ed -d "//svg:text/@stroke" \
+    > prep/$f
+
     # rm $f.1
     done
 
@@ -29,6 +34,7 @@ then
   xml2rfc $D.xml
   echo -- to pdf
   xml2rfc --pdf $D.xml
+  xml2rfc --expand $D.xml
 
   nl -ba $D.txt > $D.nl.txt
   exit
